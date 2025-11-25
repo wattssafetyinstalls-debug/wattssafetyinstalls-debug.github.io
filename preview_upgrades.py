95dd6b72037883cca6c3fb24ff02b0cda43cafc5
Justin Watts <WattsSafetyInstalls@gmail.com>
2025-11-25 01:07:51 -0600

    Fixed homepage links to use pretty URLs, created clean redirects

--- preview_upgrades.py
+++ preview_upgrades.py
@@ -1,7 +1,13 @@
+import os
 import webbrowser
 
-files_to_preview = ['index.html', 'services.html']  # Add files
+files_to_preview = ['index.html', 'services.html']  # Add more files as needed
 
 for file in files_to_preview:
-    webbrowser.open('file://' + os.path.realpath(file))
-print("Previewing files.")
+    if os.path.exists(file):
+        webbrowser.open('file://' + os.path.realpath(file))
+        print(f"Opened: {file}")
+    else:
+        print(f"File not found: {file}")
+
+print("Preview completed.")
